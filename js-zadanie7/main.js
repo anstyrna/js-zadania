4ad7b83d2c9f2d35e31e7501b6cf98487e0a2d2d
Anna Styrna <aniakatarzynas@gmail.com>
2021-04-23 19:16:53 +0200

    skończone zadanie7

---
+++ js-zadanie7/main.js
@@ -0,0 +1,10 @@
+let firstnumber = 7;
+
+let secondnumber = 28;
+
+function myFunction(num1, num2) {
+    let result = num1 + num2;
+    return result
+}
+
+console.log(myFunction(firstnumber, secondnumber));
